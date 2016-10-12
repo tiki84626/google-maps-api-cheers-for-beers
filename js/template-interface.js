aac738acf7bfea0cf24e269ce456a98372cc7627
Andrew Malkin <andymalkin@gmail.com>
2016-10-12 07:40:59 -0700

    add sass

--- js/template-interface.js
+++ js/template-interface.js
@@ -1,13 +1,15 @@
 var template = require('./../js/template.js').TemplateModule;
 
 //<!-- Back End -->
-console.log('basic javascript is functioning')
+console.log('basic javascript is functioning');
 
 //<!-- Front End  -->
 $(document).ready(function(){
-if (jQuery) {
-console.log('jQuery 3.1.0 is loaded');
-} else {
-console.log('jQuery is not loaded');
-    }
+
+  if (jQuery) {
+    console.log('jQuery 3.1.0 is loaded');
+  } else {
+    console.log('jQuery is not loaded');
+  }
+
 });
